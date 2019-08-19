78ba69e38a44d087f5d5ae38561414346e8bfa1b
커트 <curt.kim@daumkakao.com>
2019-08-19 18:04:14 +0900

    add css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import _ from 'lodash';
+import {join} from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
